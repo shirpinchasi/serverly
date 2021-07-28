5953194bd52cdb8719cd3599ad10932ae15c95f7
Shir Pinchasi <shirpi9999@gmail.com>
2021-07-28 19:34:59 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import config from "./config/development"
 import './App.css';
 
 function App() {
+  const [users,setUsers] = useState([])
+
+  useEffect(()=>{
+    async function getUsers(){
+      try{
+        const res = await fetch(config.apiUrl ,{
+          credentials : "include",
+        })
+        const fetchUsers = await res.json()
+        setUsers(fetchUsers);
+      }catch(err){
+        console.log(users);
+      }
+    }
+    getUsers()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map(user => console.log(user))}
     </div>
   );
 }
